7e8b7c760003dae4b37751740fb44026d78c59af
Andrea Gulli <andreagulli11@gmail.com>
2024-10-07 17:01:06 +0200

    add PHP logic

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
+// Dichiaro la variabili e raccolgo i dati arrivati in input dall'utente
+$passLength = isset(($_GET["numPass"])) ? $_GET["numPass"] : 0;;
+$generatedPassword = '';
 
+// Funzione che mi genera una password casuale lunga quanto scelto da lui stesso nel form
+function generatePassword($length){
+    // Dichiaro due variabili: password che sarà la stringa che compone la password generata e che verrà ritornata dalla funzione, characters essendo una stringa contente tutti i caratteri per randomizzare poi attraverso questa variabile e creare la password casuale.
+    $password = '';
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+   
+    // Ciclo for per costruitre la password generata
+    for ($i = 0; $i < $length ; $i++) {
+        $password .= $characters[rand(0, strlen($characters) - 1)];
+    }
 
+    return $password;
+}
+
+    // Creo una condizione if che verifica se l'utente ha inviato il form (premuto il bottone) ed ha inserito una lunghezza (>0) per generare la password.
+    if($passLength > 0){
+        $generatedPassword = generatePassword($passLength);
+        echo($generatedPassword);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -20,7 +41,6 @@
         </h2>
     </div>
 
-    
     <div class="card-wrapper">
         <div class="card">
             <form action="" method="get">
